68275368d7b762b13647e37ff1b4fb75c2f4cfd2
nico <lotailsmonsieur62c900@gmail.com>
2024-03-20 14:25:50 -0400

    pushy

--- breed-guesser/src/App.jsx
+++ breed-guesser/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
-import './App.css'
+import './style/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <h1>Breed Guesser</h1>
     </>
   )
 }
