8ab2839b8e6d3578b4102f9de1aeac8859b3ccc2
Berlineda <bfaurelus7@gmail.com>
2024-03-23 14:44:29 -0400

    deleted duplicated files

--- breed-guesser/src/Components/Footer.jsx
+++ breed-guesser/src/Components/Footer.jsx
@@ -1,37 +1,36 @@
-// import React from "react";
-// import './Footer.css'
+import React from "react";
+import "./Footer.css";
 
+const Footer = () => {
+  return (
+    <footer className="main-footer">
+      <div className="container footer-content">
+        <div className="footer-column">
+          <h4>Breed Guesser INC</h4>
+          <ul>
+            <li>Marcy Lab School</li>
+            <li>Brooklyn, NY</li>
+          </ul>
+        </div>
+        <div className="footer-column">
+          <h4>Creators</h4>
+          <ul>
+            <li>Berlineda Faurelus</li>
+            <li>Nico Aroca</li>
+          </ul>
+        </div>
+      </div>
+      <div className="container">
+        <hr />
+        <div className="row">
+          <p className="col-sm">
+            &copy; {new Date().getFullYear()} Breed Guesser INC | All rights
+            reserved | Terms of service | Privacy
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-// const Footer = () => {
-//     return (
-//         <footer className="main-footer">
-//         <div className="container footer-content">
-//             <div className="footer-column">
-//                 <h4>Breed Guesser INC</h4>
-//                 <ul>
-//                     <li>Marcy Lab School</li>
-//                     <li>Brooklyn, NY</li>
-//                 </ul>
-//             </div>
-//             <div className="footer-column">
-//                 <h4>Creators</h4>
-//                 <ul>
-//                     <li>Berlineda Faurelus</li>
-//                     <li>Nico Aroca</li>
-//                 </ul>
-//             </div>
-//         </div>
-//         <div className="container">
-//         <hr />
-//             <div className="row">
-//                 <p className="col-sm">
-//                     &copy; {new Date().getFullYear()} Breed Guesser INC | All rights reserved | Terms of service | Privacy
-//                 </p>
-//             </div>
-//         </div>
-//     </footer>
-// )
-    
-// }
-
-// export default Footer
+export default Footer;
