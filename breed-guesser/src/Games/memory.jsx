7f8212aca8ee344f01cfb27facca478dd389669f
nico <lotailsmonsieur62c900@gmail.com>
2024-04-01 13:56:15 -0400

    wada

--- breed-guesser/src/Games/memory.jsx
+++ breed-guesser/src/Games/memory.jsx
@@ -3,14 +3,18 @@ import Cards from "../Components/cards";
 
 const Memory = ({ newData }) => {
   return (
-    <div className={style.memoryContainer}>
-      <div>
-        <h1 className={style.h1}>Match the Doggy Pair</h1>
-        <div className={style.container}>
-          <Cards newData={newData} />
+    <>
+      <h1 className={style.h1}>Match the Doggy Pair</h1>
+      <div id='CardsContainer'>
+        <div className={style.memoryContainer}>
+          <div>
+            <div className={style.container}>
+              <Cards newData={newData} />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+      </>
   );
 };
 
--- breed-guesser/src/Pages/PageNotFound.jsx
+++ breed-guesser/src/Pages/PageNotFound.jsx
@@ -1,4 +1,12 @@
-const PageNotFound = () => <h1>Error 404: Not Found</h1>;
+const PageNotFound = () => {
+    return (
+        <>
+            <div id="component-content">
+                <h1>Error 404: Not Found</h1>
+            </div>
+        </>
+    )
+}
 
 
 export default PageNotFound
