e8506809e8320000a8bf46d8e36268a16d8a5e41
Nico <aroca.twelve@proton.me>
2025-08-06 13:05:27 -0400

    added deploy scripts + gh-pages package

--- breed-guesser/vite.config.js
+++ breed-guesser/vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // GitHub Pages expects an index.html in the root directory
-  // so just run npm build before pushing to GitHub and this will rebuild our assets to the root
-  build: { outDir: '../build' },
-  // needed for github pages just put the repo name here
-  base: '/',
+  // build: { outDir: '../build' }, 
+  base: '/Breed-Guesser',
 })
